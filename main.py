d490f48ef02f9a2ebf6ef8552de8561183d9aae5
Bartosz859 <bartek-kanownik@wp.pl>
2025-04-15 19:11:17 +0200

    wip: update user

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.model import users
-from utils.controller import get_user_info, added_user, remove_user
+from utils.controller import get_user_info, added_user, remove_user, update_user
 
 
 
@@ -10,6 +10,7 @@ def main():
     print('1 - wyświetl co u znajomych')
     print('2 - dodaj znajomego')
     print('3 - usuń znajomego')
+    print('4 - zaktualizuj dane znajomego')
     print('======================')
     while True:
         choice:str=input('wybierz opcję MENU: ')
@@ -17,6 +18,7 @@ def main():
         if choice == '1': get_user_info(users)
         if choice == '2': added_user(users)
         if choice == '3': remove_user(users)
+        if choice == '4': update_user(users)
 
 
 
